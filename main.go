800e614bdd0280db0eeb2928c3f2144bd51dfd3b
shreddedbacon <b@benjackson.email>
2022-09-28 18:27:56 +1000

    chore: colors

--- main.go
+++ main.go
@@ -49,16 +49,17 @@ func main() {
 			ls.Wipe(lights.HexToColor("00FFFF")) //cyan
 			ls.Wipe(lights.HexToColor("EB8F34")) //orange
 			ls.Wipe(lights.HexToColor("06BA90")) //teal
-			ls.Wipe(lights.HexToColor("06BA90")) //teal
 			token, err := sshtoken.GetToken("/home/pi", "lagoon-ssh.apps.shreddedbacon.com", "32222")
 			if err != nil {
-				ls.Wipe(0xFF0000)
-				ls.Wipe(0xEB8F34)
-				ls.Wipe(0xFF0000)
-				ls.Wipe(0xEB8F34)
-				ls.Wipe(0x0690BA)
+				ls.Wipe(lights.HexToColor("FF0000")) //red
+				ls.Wipe(lights.HexToColor("EB8F34")) //orange
+				ls.Wipe(lights.HexToColor("FFFF00")) //yellow
+				ls.Wipe(lights.HexToColor("FF0000")) //red
+				ls.Wipe(lights.HexToColor("EB8F34")) //orange
+				ls.Wipe(lights.HexToColor("FFFF00")) //yellow
 				fmt.Println(err)
 				time.Sleep(time.Second)
+				ls.Wipe(lights.HexToColor("06BA90")) //teal
 				continue
 			}
 			ctx := context.Background()
@@ -73,21 +74,25 @@ func main() {
 			l := lclient.New(lagoonAPI, token, "deploy2lights", false)
 			deployment, err := lagoon.DeployLatest(ctx, deploy, l)
 			if err != nil {
-				ls.Wipe(0xFF0000)
-				ls.Wipe(0xEB8F34)
-				ls.Wipe(0xFF0000)
-				ls.Wipe(0xEB8F34)
-				ls.Wipe(0x0690BA)
+				ls.Wipe(lights.HexToColor("FF0000")) //red
+				ls.Wipe(lights.HexToColor("EB8F34")) //orange
+				ls.Wipe(lights.HexToColor("FFFF00")) //yellow
+				ls.Wipe(lights.HexToColor("FF0000")) //red
+				ls.Wipe(lights.HexToColor("EB8F34")) //orange
+				ls.Wipe(lights.HexToColor("FFFF00")) //yellow
 				fmt.Println(err)
 				time.Sleep(time.Second)
+				ls.Wipe(lights.HexToColor("06BA90")) //teal
 				continue
 			}
 			fmt.Println(deployment)
-			ls.Wipe(0x00FF00)
-			ls.Wipe(0x00FFFF)
-			ls.Wipe(0x00FF00)
-			ls.Wipe(0x00FFFF)
-			ls.Wipe(0x0690BA)
+			ls.Wipe(lights.HexToColor("00FF00")) //green
+			ls.Wipe(lights.HexToColor("7BA832")) //lighter green
+			ls.Wipe(lights.HexToColor("48D99F")) //teal green
+			ls.Wipe(lights.HexToColor("00FF00")) //green
+			ls.Wipe(lights.HexToColor("7BA832")) //lighter green
+			ls.Wipe(lights.HexToColor("48D99F")) //teal green
+			ls.Wipe(lights.HexToColor("06BA90")) //teal
 		}
 		time.Sleep(time.Second)
 	}
