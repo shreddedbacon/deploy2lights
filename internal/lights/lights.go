47facf0a2ba3bfb13c5d030d8ece9389576e55e3
shreddedbacon <b@benjackson.email>
2022-10-14 18:57:23 +1100

    chore: use a loop for led strip

--- internal/lights/lights.go
+++ internal/lights/lights.go
@@ -2,6 +2,7 @@ package lights
 
 import (
 	"strconv"
+	"sync"
 	"time"
 
 	ws2811 "github.com/rpi-ws281x/rpi-ws281x-go"
@@ -10,7 +11,6 @@ import (
 type Flare struct {
 	Position int
 	Color    uint32
-	Speed    int
 }
 
 type wsEngine interface {
@@ -25,10 +25,12 @@ type LED struct {
 	WS wsEngine
 }
 
+// helper function to convert a rgb uint8s to a uint32 for colors
 func rgbToColor(r uint8, g uint8, b uint8) uint32 {
 	return uint32(uint32(r)<<16 | uint32(g)<<8 | uint32(b))
 }
 
+// helper function to convert a hex string to a uint32 for colors
 func HexToColor(hex string) uint32 {
 	values, err := strconv.ParseUint(string(hex), 16, 32)
 	if err != nil {
@@ -42,6 +44,8 @@ func Setup(brightness, ledCount int, stripType string) (*LED, error) {
 	opt := ws2811.DefaultOptions
 	opt.Channels[0].Brightness = brightness
 	opt.Channels[0].LedCount = ledCount
+
+	// check the LED strip type to determine the correct RGB order for the strip
 	switch stripType {
 	case "RBG":
 		opt.Channels[0].StripeType = ws2811.WS2811StripRBG
@@ -72,8 +76,10 @@ func Setup(brightness, ledCount int, stripType string) (*LED, error) {
 }
 
 func (ls *LED) Startup() {
-	// startup animation, once this is complete, builds can start
-	ls.Wipe(HexToColor("06BA90")) //teal
+	// startup animation, verify all leds work
+	ls.Display(HexToColor("06BA90"))
+	time.Sleep(2 * time.Second)
+	ls.Display(HexToColor("000000"))
 }
 
 func (ls *LED) Wipe(color uint32) error {
@@ -87,21 +93,57 @@ func (ls *LED) Wipe(color uint32) error {
 	return nil
 }
 
-func (ls *LED) Display(ledValues *map[string]Flare) error {
+func (ls *LED) Display(color uint32) error {
 	for i := 0; i < len(ls.WS.Leds(0)); i++ {
-		ls.WS.Leds(0)[i] = HexToColor("000000")
-	}
-
-	for k, v := range *ledValues {
-		if (*ledValues)[k].Position == len(ls.WS.Leds(0)) {
-			delete(*ledValues, k)
-		} else {
-			ls.WS.Leds(0)[v.Position] = v.Color
-		}
+		ls.WS.Leds(0)[i] = color
 	}
 	if err := ls.WS.Render(); err != nil {
 		return err
 	}
-	time.Sleep(10 * time.Millisecond)
+	return nil
+}
+
+func (ls *LED) DisplayFlares(delayms int, ledValues *sync.Map) error {
+	// clear out the led strip to start
+	for i := 0; i < len(ls.WS.Leds(0)); i++ {
+		ls.WS.Leds(0)[i] = HexToColor("000000")
+	}
+
+	// iterate over the flare map to set the lights up
+	ledValues.Range(func(key, value interface{}) bool {
+		if value.(Flare).Position == len(ls.WS.Leds(0)) {
+			ls.WS.Leds(0)[value.(Flare).Position-1] = value.(Flare).Color
+			ls.WS.Leds(0)[value.(Flare).Position-2] = value.(Flare).Color
+			ls.WS.Leds(0)[value.(Flare).Position-3] = value.(Flare).Color
+			ledValues.Delete(key)
+		} else if value.(Flare).Position < len(ls.WS.Leds(0)) {
+			ls.WS.Leds(0)[value.(Flare).Position] = value.(Flare).Color
+			if value.(Flare).Position > 0 {
+				ls.WS.Leds(0)[value.(Flare).Position-1] = value.(Flare).Color
+			}
+			if value.(Flare).Position > 1 {
+				ls.WS.Leds(0)[value.(Flare).Position-2] = value.(Flare).Color
+			}
+			if value.(Flare).Position > 2 {
+				ls.WS.Leds(0)[value.(Flare).Position-3] = value.(Flare).Color
+			}
+		}
+		return true
+	})
+	// render the light strip
+	if err := ls.WS.Render(); err != nil {
+		return err
+	}
+	// once the strip has rendered, increment all flares by 1
+	ledValues.Range(func(key, value interface{}) bool {
+		ledValues.Store(key, Flare{
+			Position: value.(Flare).Position + 1,
+			Color:    value.(Flare).Color,
+		})
+		return true
+	})
+
+	// add a delay if required (shorter led strips will move flares faster than longer ones will)
+	time.Sleep(time.Duration(delayms) * time.Millisecond)
 	return nil
 }
--- main.go
+++ main.go
@@ -27,6 +27,7 @@ func main() {
 	var ledBrightness int
 	var lagoonAPI, stripType, projectName, environmentName string
 	var sshHost, sshPort, sshKey string
+	var displayDuration int
 
 	flag.StringVar(&lagoonAPI, "lagoon-api", "https://lagoon-api.apps.shreddedbacon.com/graphql",
 		"The lagoon API url.")
@@ -36,6 +37,8 @@ func main() {
 		"The lagoon project name.")
 	flag.StringVar(&environmentName, "environment-name", "master",
 		"The lagoon environment name.")
+	flag.IntVar(&displayDuration, "led-duration-interval", 1,
+		"The duration between render intervals in ms.")
 
 	flag.IntVar(&ledCount, "led-count", 6,
 		"The total number of LEDs.")
@@ -77,23 +80,37 @@ func main() {
 
 	defer pin.Detect(rpio.NoEdge)
 
+	builds := &[]string{}
+
+	go func() {
+		for {
+			if len(*builds) > 0 {
+				for _, c := range *builds {
+					ls.Wipe(lights.HexToColor(c)) //blue
+				}
+			}
+		}
+	}()
+
 	for {
 		if pin.EdgeDetected() {
 			fmt.Println("button pressed")
-			ls.Wipe(lights.HexToColor("0000FF")) //blue
-			ls.Wipe(lights.HexToColor("06BA90")) //teal
-			ls.Wipe(lights.HexToColor("48D99F")) //teal green
+			builds = &[]string{"0000FF", "06BA90", "48D99F"}
+			// ls.Wipe(lights.HexToColor("0000FF")) //blue
+			// ls.Wipe(lights.HexToColor("06BA90")) //teal
+			// ls.Wipe(lights.HexToColor("48D99F")) //teal green
 			token := ""
 			err = sshtoken.ValidateOrRefreshToken(sshKey, sshHost, sshPort, &token)
 			if err != nil {
-				ls.Wipe(lights.HexToColor("FF0000")) //red
-				ls.Wipe(lights.HexToColor("EB8F34")) //orange
-				ls.Wipe(lights.HexToColor("FFFF00")) //yellow
-				ls.Wipe(lights.HexToColor("FF0000")) //red
-				ls.Wipe(lights.HexToColor("EB8F34")) //orange
-				ls.Wipe(lights.HexToColor("FFFF00")) //yellow
 				fmt.Println("generate token error:", err)
-				time.Sleep(time.Second)
+				// ls.Wipe(lights.HexToColor("FF0000")) //red
+				// ls.Wipe(lights.HexToColor("EB8F34")) //orange
+				// ls.Wipe(lights.HexToColor("FFFF00")) //yellow
+				// ls.Wipe(lights.HexToColor("FF0000")) //red
+				// ls.Wipe(lights.HexToColor("EB8F34")) //orange
+				// ls.Wipe(lights.HexToColor("FFFF00")) //yellow
+				builds = &[]string{"FF0000", "EB8F34", "FFFF00"}
+				time.Sleep(time.Second * 5)
 				ls.Wipe(lights.HexToColor("06BA90")) //teal
 				continue
 			}
@@ -117,59 +134,36 @@ func main() {
 			l := lclient.New(lagoonAPI, "deploy2lights", &token, false)
 			project, err := lagoon.GetMinimalProjectByName(ctx, projectName, l)
 			if err != nil {
-				ls.Wipe(lights.HexToColor("FF0000")) //red
-				ls.Wipe(lights.HexToColor("EB8F34")) //orange
-				ls.Wipe(lights.HexToColor("FFFF00")) //yellow
-				ls.Wipe(lights.HexToColor("FF0000")) //red
-				ls.Wipe(lights.HexToColor("EB8F34")) //orange
-				ls.Wipe(lights.HexToColor("FFFF00")) //yellow
 				fmt.Println("project get error:", err)
-				time.Sleep(time.Second)
+				builds = &[]string{"FF0000", "EB8F34", "FFFF00"}
+				time.Sleep(time.Second * 5)
 				ls.Wipe(lights.HexToColor("06BA90")) //teal
 				continue
 			}
 			deployment, err := lagoon.DeployLatest(ctx, deploy, l)
 			if err != nil {
-				ls.Wipe(lights.HexToColor("FF0000")) //red
-				ls.Wipe(lights.HexToColor("EB8F34")) //orange
-				ls.Wipe(lights.HexToColor("FFFF00")) //yellow
-				ls.Wipe(lights.HexToColor("FF0000")) //red
-				ls.Wipe(lights.HexToColor("EB8F34")) //orange
-				ls.Wipe(lights.HexToColor("FFFF00")) //yellow
 				fmt.Println("deploy error:", err)
-				time.Sleep(time.Second)
+				builds = &[]string{"FF0000", "EB8F34", "FFFF00"}
+				time.Sleep(time.Second * 5)
 				ls.Wipe(lights.HexToColor("06BA90")) //teal
 				continue
 			}
-			ls.Wipe(lights.HexToColor("0000FF")) //blue
-			ls.Wipe(lights.HexToColor("06BA90")) //teal
-			ls.Wipe(lights.HexToColor("48D99F")) //teal green
 			fmt.Println("started", deployment.DeployEnvironmentLatest, project.Name, project.ID)
 			timeout := 1
 			for timeout <= 600 {
 				err := sshtoken.ValidateOrRefreshToken(sshKey, sshHost, sshPort, &token)
 				if err != nil {
-					ls.Wipe(lights.HexToColor("FF0000")) //red
-					ls.Wipe(lights.HexToColor("EB8F34")) //orange
-					ls.Wipe(lights.HexToColor("FFFF00")) //yellow
-					ls.Wipe(lights.HexToColor("FF0000")) //red
-					ls.Wipe(lights.HexToColor("EB8F34")) //orange
-					ls.Wipe(lights.HexToColor("FFFF00")) //yellow
 					fmt.Println("token validation error:", err)
-					time.Sleep(time.Second)
+					builds = &[]string{"FF0000", "EB8F34", "FFFF00"}
+					time.Sleep(time.Second * 5)
 					ls.Wipe(lights.HexToColor("06BA90")) //teal
 					break
 				}
 				environment, err := lagoon.GetDeploymentsByEnvironment(ctx, project.ID, environmentName, l)
 				if err != nil {
-					ls.Wipe(lights.HexToColor("FF0000")) //red
-					ls.Wipe(lights.HexToColor("EB8F34")) //orange
-					ls.Wipe(lights.HexToColor("FFFF00")) //yellow
-					ls.Wipe(lights.HexToColor("FF0000")) //red
-					ls.Wipe(lights.HexToColor("EB8F34")) //orange
-					ls.Wipe(lights.HexToColor("FFFF00")) //yellow
 					fmt.Println("list deploy error:", err)
-					time.Sleep(time.Second)
+					builds = &[]string{"FF0000", "EB8F34", "FFFF00"}
+					time.Sleep(time.Second * 5)
 					ls.Wipe(lights.HexToColor("06BA90")) //teal
 					break
 				}
@@ -177,46 +171,58 @@ func main() {
 				for _, deploy := range environment.Deployments {
 					if deploy.Name == deployment.DeployEnvironmentLatest {
 						fmt.Println(deploy.Name, deploy.Status)
-						wipeCount := 4
+						// wipeCount := 4
 						switch deploy.Status {
 						case "new":
-							for j := 1; j <= wipeCount; j++ {
-								ls.Wipe(lights.HexToColor("6200ff")) //purple
-								ls.Wipe(lights.HexToColor("a77bed")) //lighter purple
-								ls.Wipe(lights.HexToColor("8249ab")) //lighter again purple
-							}
+							// for j := 1; j <= wipeCount; j++ {
+							// ls.Wipe(lights.HexToColor("6200ff")) //purple
+							// ls.Wipe(lights.HexToColor("a77bed")) //lighter purple
+							// ls.Wipe(lights.HexToColor("8249ab")) //lighter again purple
+							builds = &[]string{"6200FF", "A77BED", "8249AB"}
+							time.Sleep(time.Second * 5)
+							// }
 						case "pending":
-							for j := 1; j <= wipeCount; j++ {
-								ls.Wipe(lights.HexToColor("f542b6")) //pink
-								ls.Wipe(lights.HexToColor("87095b")) //darker pink
-								ls.Wipe(lights.HexToColor("e681c2")) //lighter pink
-							}
+							// for j := 1; j <= wipeCount; j++ {
+							// ls.Wipe(lights.HexToColor("f542b6")) //pink
+							// ls.Wipe(lights.HexToColor("87095b")) //darker pink
+							// ls.Wipe(lights.HexToColor("e681c2")) //lighter pink
+							builds = &[]string{"F542B6", "87095B", "E681C2"}
+							time.Sleep(time.Second * 5)
+							// }
 						case "running":
-							for j := 1; j <= wipeCount; j++ {
-								ls.Wipe(lights.HexToColor("00f7ff")) //light blue
-								ls.Wipe(lights.HexToColor("027399")) //cyan blue
-								ls.Wipe(lights.HexToColor("2d8385")) //teal blue
-							}
+							// for j := 1; j <= wipeCount; j++ {
+							// 	ls.Wipe(lights.HexToColor("00f7ff")) //light blue
+							// 	ls.Wipe(lights.HexToColor("027399")) //cyan blue
+							// 	ls.Wipe(lights.HexToColor("2d8385")) //teal blue
+							// }
+							builds = &[]string{"00F7FF", "027399", "2D8385"}
+							time.Sleep(time.Second * 5)
 						case "complete":
-							for j := 1; j <= 10; j++ {
-								ls.Wipe(lights.HexToColor("00FF00")) //green
-								ls.Wipe(lights.HexToColor("7BA832")) //lighter green
-								ls.Wipe(lights.HexToColor("BBFF00")) //teal green
-							}
+							// for j := 1; j <= 10; j++ {
+							// 	ls.Wipe(lights.HexToColor("00FF00")) //green
+							// 	ls.Wipe(lights.HexToColor("7BA832")) //lighter green
+							// 	ls.Wipe(lights.HexToColor("BBFF00")) //teal green
+							// }
+							builds = &[]string{"00FF00", "7BA832", "BBFF00"}
+							time.Sleep(time.Second * 10)
 							breakout = true
 						case "failed":
-							for j := 1; j <= 10; j++ {
-								ls.Wipe(lights.HexToColor("FF0000")) //red
-								ls.Wipe(lights.HexToColor("EB8F34")) //orange
-								ls.Wipe(lights.HexToColor("FFFF00")) //yellow
-							}
+							// for j := 1; j <= 10; j++ {
+							// 	ls.Wipe(lights.HexToColor("FF0000")) //red
+							// 	ls.Wipe(lights.HexToColor("EB8F34")) //orange
+							// 	ls.Wipe(lights.HexToColor("FFFF00")) //yellow
+							// }
+							builds = &[]string{"FF0000", "EB8F34", "FFFF00"}
+							time.Sleep(time.Second * 10)
 							breakout = true
 						case "cancelled":
-							for j := 1; j <= 10; j++ {
-								ls.Wipe(lights.HexToColor("FF0000")) //red
-								ls.Wipe(lights.HexToColor("EB8F34")) //orange
-								ls.Wipe(lights.HexToColor("FFFF00")) //yellow
-							}
+							// for j := 1; j <= 10; j++ {
+							// 	ls.Wipe(lights.HexToColor("FF0000")) //red
+							// 	ls.Wipe(lights.HexToColor("EB8F34")) //orange
+							// 	ls.Wipe(lights.HexToColor("FFFF00")) //yellow
+							// }
+							builds = &[]string{"FF0000", "EB8F34", "FFFF00"}
+							time.Sleep(time.Second * 10)
 							breakout = true
 						}
 					}
@@ -227,6 +233,7 @@ func main() {
 				// time.Sleep(5 * time.Second) // sleep for 5 seconds, wait total 250 seconds (token will expire)
 				timeout++
 			}
+			builds = &[]string{}
 			ls.Wipe(lights.HexToColor("06BA90")) //teal
 		}
 		time.Sleep(time.Second)
